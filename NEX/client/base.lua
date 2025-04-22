a765a3d0693bf54e2d608994c78472dd022aee31
Ianis Catalin <73122512+IanisCatalin15@users.noreply.github.com>
2025-04-22 11:45:05 +0300

    Delete NEX/client directory

--- NEX/client/base.lua
+++
@@ -1,7 +0,0 @@
--- client/base.lua
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(NEXShared.Config.player_state.update_interval * 1000)
-        TriggerServerEvent(NEXShared.Constants.EVENT_PREFIX .. 'player:stateUpdate', GetEntityCoords(PlayerPedId()))
-    end
-end)
